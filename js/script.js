23b8552f67c3144d97daa582e626081a5f426449
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-08-13 18:42:29 +0300

    header adaptive

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ $('.slickBlock').slick({
 	autoplay: true,
 	autoplaySpeed: 2000,
 });
+
+
+const menu = document.querySelector('#menu-btn');
+const headerLinks = document.querySelector('.header__links');
+const menuActive = document.querySelector('.header__menu');
+
+menu.addEventListener('click', function () { 
+	headerLinks.classList.toggle('header__links--active');
+	menuActive.classList.toggle('active');
+});
