f1ab3a2315dc3e60720bc28879581fe20d3d99b4
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-08-13 22:27:48 +0300

    clients adaptive

--- js/script.js
+++ js/script.js
@@ -6,6 +6,40 @@ $('.slickBlock').slick({
 	dots: true,
 	autoplay: true,
 	autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 435,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
 });
 
 
